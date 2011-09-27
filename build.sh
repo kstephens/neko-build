e4c944230e757a637dd93d6b682a4af7355c30de
Kurt Stephens <ks.github@kurtstephens.com>
2011-09-27 18:13:56 -0500

    Actually configure, build and test.

--- build.sh
+++ build.sh
@@ -7,14 +7,17 @@ if [ -d /opt/local ]
 then
   export CFLAGS="$CFLAGS -I/opt/local/include" LDFLAGS='-L/opt/local/lib'
 fi
+# prereqs:
+# apt-get install 
 branch="$(basename "$(sed -e 's@ref: @@' $base_dir/ruby/.git/HEAD)")"
 mkdir -p $base_dir/build/ruby
 prefix="$(cd $base_dir/build && mkdir -p ruby/$branch && cd -P ruby/$branch && /bin/pwd)"
 set -e
 (
 cd $base_dir/ruby
-echo ./configure --prefix="${prefix}"
-echo make clean
-echo make
-)
+./configure --prefix="${prefix}"
+make clean
+make
+make test
+) || exit $?
 
