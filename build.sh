2498a830f5d231e0a4a8844243deaec449f37f7c
Kurt Stephens <ks.github@kurtstephens.com>
2011-09-27 18:17:00 -0500

    Run autoconf, if ./configure does not exist.

--- build.sh
+++ build.sh
@@ -8,13 +8,14 @@ then
   export CFLAGS="$CFLAGS -I/opt/local/include" LDFLAGS='-L/opt/local/lib'
 fi
 # prereqs:
-# apt-get install 
+# apt-get install libgdbm-dev libreadline-dev autoconf
 branch="$(basename "$(sed -e 's@ref: @@' $base_dir/ruby/.git/HEAD)")"
 mkdir -p $base_dir/build/ruby
 prefix="$(cd $base_dir/build && mkdir -p ruby/$branch && cd -P ruby/$branch && /bin/pwd)"
 set -e
 (
 cd $base_dir/ruby
+[ -f ./configure ] || autoconf
 ./configure --prefix="${prefix}"
 make clean
 make
