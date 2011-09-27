2db61d91394244f74a11619b950e9d3cad16c423
Kurt Stephens <ks.github@kurtstephens.com>
2011-09-27 17:19:05 -0500

    Initial version.

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh 
+set -x
+#alias cd='builtin cd'
+base_dir="$(cd -P "$(dirname "$0")/.." && /bin/pwd)"
+export CFLAGS='-O2'
+if [ -d /opt/local ]
+then
+  export CFLAGS="$CFLAGS -I/opt/local/include" LDFLAGS='-L/opt/local/lib'
+fi
+branch="$(basename "$(sed -e 's@ref: @@' $base_dir/ruby/.git/HEAD)")"
+mkdir -p $base_dir/build
+prefix="$(cd $base_dir/build && mkdir -p $branch && cd -P $branch && /bin/pwd)"
+set -e
+(
+cd $base_dir/ruby
+echo ./configure --prefix="${prefix}"
+echo make clean
+echo make
+)
+
