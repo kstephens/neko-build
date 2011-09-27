b9c84d54164dd3c942a53ee8abae8b7734224f8d
Kurt Stephens <ks.github@kurtstephens.com>
2011-09-27 17:21:35 -0500

    Use build/ruby/BRANCH/ for prefix.

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ then
   export CFLAGS="$CFLAGS -I/opt/local/include" LDFLAGS='-L/opt/local/lib'
 fi
 branch="$(basename "$(sed -e 's@ref: @@' $base_dir/ruby/.git/HEAD)")"
-mkdir -p $base_dir/build
-prefix="$(cd $base_dir/build && mkdir -p $branch && cd -P $branch && /bin/pwd)"
+mkdir -p $base_dir/build/ruby
+prefix="$(cd $base_dir/build && mkdir -p ruby/$branch && cd -P ruby/$branch && /bin/pwd)"
 set -e
 (
 cd $base_dir/ruby
